f289e899973145e80f4518d9a40e2889c14b47d2
jcarlsso <jamescarlsson21@gmail.com>
2020-07-11 21:45:11 -0500

    date_time data type change

--- main.py
+++ main.py
@@ -1,49 +1,33 @@
 import pandas as pd
+import json
+import datetime as dt
 
-df = pd.read_json(r'/MyData/endsong_0.json')
-print(df)
-
-
-
-
-
+# json_1 = pd.read_json(r'MyData/endsong_0.json')
+# json_2 = pd.read_json(r'MyData/endsong_1.json')
+# json_3 = pd.read_json(r'MyData/endsong_2.json')
 
 
 
+# x = pd.Series(json_3['ts'])
+# for line in x:
+#     new = line.split("T")
+#     if new[0] == "2019-12-05":
+#         print(['master_metadata_album_album_name'])
 
 
+with open("MyData/endsong_0.json", "r", encoding="utf8") as file1:
+    jdata1 = json.load(file1)
 
+# print(json.dumps(jdata1, indent=2))
 
+def before2018():
+    for data in jdata1:
+        jdata1_cleaned = data['ts'], data['master_metadata_album_artist_name'], data['master_metadata_album_album_name'], data['master_metadata_track_name']
+        # date_time = dt.datetime.strptime(data['ts'], '%Y-%m-%dT%H:%M:%SZ')
+        x = []
+        if dt.datetime.strptime(data['ts'], '%Y-%m-%dT%H:%M:%SZ') < dt.datetime(2018,12,27,0,0,0,):
+            x.append(data)
+            print(x)
 
+before2018()
 
-
-
-
-
-
-# import json
-
-# with open("MyData/endsong_0.json", "r", encoding="utf8") as file1:
-#     jdata1 = json.load(file1)
-
-# #print(json.dumps(jdata_1, indent=2))
-# jdata1_cleaned_list = []
-
-# for data in jdata1:
-#     #if data['ts'] == '2016-*'
-#     jdata1_cleaned = data['ts'], data['master_metadata_album_artist_name'], data['master_metadata_album_album_name'], data['master_metadata_track_name']
-#     jdata1_cleaned_list.append(jdata1_cleaned)
-
-# print(jdata1_cleaned_list)
-
-#     # for key, value in data.items():
-
-#         # if key == "master_metadata_album_album_name" and value == "DAMN.":
-#         #     print(key,":", value)
-
-
-
-
-# # for line in jdata_1:
-# #     if ts == "2019-01-25T18:44:47Z":
-# #         print(data)
